d2ce440a12dbc7ed4507a3803e5b0a11d7d96305
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-03-12 13:00:34 +0000

    commonize keylayouts

--- device-common.mk
+++ device-common.mk
@@ -216,6 +216,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.gatekeeper@1.0-service \
 	android.hardware.gatekeeper@1.0-impl
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+	$(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
+	$(COMMON_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
+	$(COMMON_PATH)/configs/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
+	$(COMMON_PATH)/configs/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc	
 	
 # Keymaster
 PRODUCT_PACKAGES += \
