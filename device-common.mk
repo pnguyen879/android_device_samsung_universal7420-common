75bafeee76f8cbede386e29595c8d4d30285d2e0
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-03-18 15:40:28 +0000

    Inherit nfc stuff

--- device-common.mk
+++ device-common.mk
@@ -267,6 +267,11 @@ PRODUCT_PACKAGES += \
 	com.android.nfc_extras \
 	android.hardware.nfc@1.0-impl \
 	android.hardware.nfc@1.0-service
+	
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/configs/nfc/libnfc-sec-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-hal.conf \
+	$(COMMON_PATH)/configs/nfc/libnfc-sec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec.conf \
+	$(COMMON_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # OMX
 PRODUCT_PACKAGES += \
