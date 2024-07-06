7819de7ed417f07a28dfc4c8b1e5b8ce8e8100c6
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-07-06 11:46:33 +0100

    Inherit properties from vendor

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,3 +188,6 @@ WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+
+# Inherit from the proprietary version
+include vendor/samsung/universal7420-common/BoardConfigVendor.mk
