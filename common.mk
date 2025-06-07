5e4afa49258a885497fb4a4f8c7ee5c4ad01a5b0
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    universal7420-common: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- common.mk
+++ common.mk
@@ -368,7 +368,7 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # Touch
 PRODUCT_PACKAGES += \
-	vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 # Thermal
 PRODUCT_PACKAGES += \
